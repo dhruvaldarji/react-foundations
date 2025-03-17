371077994d4b23f10bfa956eb2049a15bdff23d0
Dhruval Darji <ddarji@onetrust.com>
2025-03-17 17:50:16 -0400

    build: add support for Typescript compiler and a Vite server

---
+++ src/react-foundations.tsx
@@ -0,0 +1,26 @@
+function Header({ title }: { title: string }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <button>Like</button>
+    </div>
+  );
+}
+
+export function ReactFoundations() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
